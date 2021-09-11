21e136ce1c1923c634497201ec0e974d29fc4427
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2021-09-11 14:56:32 -0400

    initial testing file

--- src/index.js
+++ src/index.js
@@ -3,30 +3,55 @@ import test from './component/test.js';
 
 // overrideConsole({context: '../'});
 // overrideConsole({ depth: 5 });
-overrideConsole({});
+// overrideConsole({});
 
 // console.log('test');
 // console.debug('test');
 // console.warn('test');
 // console.error('test');
 
-// const very_nested_obj = {
-// 	y: {
-// 		a: {
-// 			b: {
-// 				c: {
-// 					d: {
-// 						e: {
-// 							f: {
-// 								f: 's',
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 		},
-// 	},
-// };
+const very_nested_obj = {
+	y: {
+		a: {
+			b: {
+				c: {
+					d: {
+						e: {
+							f: {
+								f: 's',
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
 // console.log(very_nested_obj);
 
-test();
+// test();
+
+overrideConsole({
+	context: './',
+	obj_depth: null, // object nested depth that is shown in console log, null means show absolutely everything
+	show_timestamp: true,
+	filter: {
+		console_method: ['debug', 'log', 'warn', 'error'],
+		output: '',
+	},
+});
+
+const arr = ['X'];
+const obj = { lol: 'x' };
+class Rectangle {
+	constructor(height, width) {
+		this.height = height;
+		this.width = width;
+	}
+}
+const func = () => {
+	console.log('test');
+};
+const string = '';
+// console.log(arr, obj, Rectangle, func,string);
+console.log([very_nested_obj]);
